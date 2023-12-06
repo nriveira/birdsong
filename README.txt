7f851e202ed0748668edfcff713f71c716f78793
nriveira <47254070+nriveira@users.noreply.github.com>
2023-12-05 16:58:02 -0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,66 @@
+List of all programs and their purposes
+
+SAP_features_test.ipynb
+SAP_features.ipynb/.py
+feature_imp.ipynb
+
+syllable_sorting.ipynb
+automated_umap_syllables.ipynb
+umap_syllables.ipynb
+song_spikes.ipynb
+
+videoFormatterBirdsong.ipynb
+time_alignment.ipynb
+
+analysis_pipeline.ipynb
+umapping_songs.ipynb
+
+# Feature Extraction Pipeline #
+Features based on Sound Analysis Pro (2011) and Librosa
+    * SAP_features.ipynb
+        Preliminary implementation of SAP features in a notebook:
+            STFT and Spectral Derivative (Primitive T/F Derivative)
+            Amplitude (log based amplitude)
+            Pitch Estimation (Using Peak/Mean/Yin Methods)
+            Wiener entropy (normalized)
+            Frequency Modulation
+            Amplitude Modulation
+            Spectral Continuity (Weird and parameter dependent)
+            Spectral Contrast (From Librosa)
+            Mel Spectrogram (Compared to Regular Spectrogram/Spectrogram Derivative)
+
+    * SAP_features.py
+        Python script for features validated in SAP_features.ipynb
+
+    * SAP_features_test.ipynb
+        Testing notebook for SAP_features.py
+
+    * feature_imp.ipynb
+        Combining all features into one notebook, copying the implementation from SAP and adding librosa features
+
+# Using Features to Separate Syllables #
+    * syllable_sorting.ipynb
+        Segment syllables according to spectral entropy, and store as a pkl file
+
+# Feature Embedding # 
+    * automated_umap_syllables.ipynb
+        Get segmented syllables from previous code, extract features, and plot them onto UMAP embedding
+
+    * umap_syllables.ipynb
+        Use hand-segmented data with spectral features to get umap embedding
+
+# Syllable Analysis #
+    * song_spikes.ipynb
+        Look for patterns and connections between syllables (preliminary)
+
+# Tools #
+    * videoFormatterBirdsong.ipynb
+        Combine all recording data for one day into a single video, renaming all appropriate files
+
+    * time_alignment.ipynb
+        First attempt at aligning neural data to the audio recordings
+
+
+
+
+
---
+++ SAP_features.py
@@ -0,0 +1,119 @@
+# Create a deployable version of all SAP features
+from re import S
+import numpy as np
+import librosa
+import scipy.signal as signal
+
+from scipy.fft import fft, ifft, fftfreq, fftshift
+
+# Create an object that will store all of the song features
+class SAP_features:
+    # Create a constructor for the object
+    def __init__(self, y, fs, window_size=1024, hop_length=128, mel_spec=False):
+        # Song variables
+        self.y = y.astype(float)
+        self.fs = fs
+
+        # Variables associated with features
+        self.window_size = window_size
+        self.hop_length = hop_length
+
+        # Calculate all features
+        t, f, Sxx, gof, fm, am, sd, ent, amp = calculate_features(self.y, self.window_size, self.hop_length, fs)
+
+        # Create a sonogram
+        self.t = t
+        self.f = f 
+        self.sonogram = Sxx
+        self.spectral_derivative = sd
+
+        # Feature Derivatives 
+        self.goodness_of_fit = gof
+
+        # Amplitude
+        self.amplitude = amp
+
+        # Pitch Estimation Measures RAW
+        self.peak_freq = self.f[np.argmax(self.sonogram, axis=0)]
+        self.mean_freq = self.f.dot(self.sonogram**2) / np.sum(self.sonogram**2, axis=0)
+
+        # Pitch Estimation Measures using YIN algorithm
+        self.yin_freq = librosa.yin(y=self.y, fmin=50, fmax=2000, sr=self.fs, win_length=self.window_size, hop_length=self.hop_length)
+
+        # Wiener Entropy
+        self.entropy = ent
+
+        # Modulation
+        self.frequency_modulation = fm
+        self.amplitude_modulation = am
+        
+# Helper function
+# Actually calculate all features to use in SAP structure
+def calculate_features(x, window_size, hop_length, fs, num_tapers=2):
+    tapers = signal.windows.dpss(window_size, 1.5, 2)
+    size = len(x)
+    f_notShifted = fftfreq(window_size, 1/fs)
+    f = fftshift(f_notShifted)
+    f_index = f > 0
+
+    sonogram = np.zeros((f_index.sum(), np.floor(size / hop_length).astype(int)))
+    freq_deriv = np.zeros((f_index.sum(), np.floor(size / hop_length).astype(int)))
+    time_deriv = np.zeros((f_index.sum(), np.floor(size / hop_length).astype(int)))
+
+    goodness_of_fit = np.zeros(np.floor(size / hop_length).astype(int))
+    frequency_modulation = np.zeros(np.floor(size / hop_length).astype(int))
+    amplitude_modulation = np.zeros(np.floor(size / hop_length).astype(int))
+    spectral_derivative = np.zeros((f_index.sum(), np.floor(size / hop_length).astype(int)))
+    entropy = np.zeros(np.floor(size / hop_length).astype(int))
+    amplitude = np.zeros(np.floor(size / hop_length).astype(int))
+
+    wav_smp = np.arange(size-window_size, step=hop_length).astype(int)
+    t = np.arange(np.floor(size / hop_length)) *(hop_length/fs)
+
+    for i in range(len(wav_smp)):
+        samps = np.arange(wav_smp[i], np.floor(wav_smp[i] + window_size).astype(int))
+        window1 = x[samps] * tapers[0]
+        window2 = x[samps] * tapers[1]
+
+        # If the window has values, calculate the cepstrum
+        if(window1.any()):
+            real_cepstrum = fftshift(np.real(ifft(np.log10(fft(window1)))))
+            goodness_of_fit[i] = np.max(real_cepstrum[f_index])
+        else:
+            goodness_of_fit[i] = 0
+        
+        powSpect1 = fftshift(fft(window1))
+        powSpect2 = fftshift(fft(window2))
+
+        r1 = (np.abs(powSpect1) + np.abs(powSpect2))**2
+        sonogram[:,i] = r1[f_index]
+
+        # Getting time and frequency derivatives
+        fR1 = np.real(powSpect1[f_index])
+        fi1 = np.imag(powSpect1[f_index])
+        fR2 = np.real(powSpect2[f_index])
+        fi2 = np.imag(powSpect2[f_index])
+
+        time_deriv[:,i] = -fR1*fR2 - fi1*fi2
+        freq_deriv[:,i] = fi1*fR2 - fR1*fi2
+
+        # Getting frequnecy modulation
+        frequency_modulation[i] = np.arctan((np.max(time_deriv[:,i])/np.max(freq_deriv[:,i]))+0.1)
+        amplitude_modulation[i] = time_deriv[:,i].sum()
+
+        # Solving for spectral derivatives
+        cFM = np.cos(frequency_modulation[i])
+        sFM = np.sin(frequency_modulation[i])
+        spectral_derivative[:,i] = time_deriv[:,i].dot(cFM) + freq_deriv[:,i].dot(sFM)
+
+        # Compute entropy
+        sumLog = np.sum(np.log(sonogram[10:,i])) / (f_index.sum()-10)
+        sumSon = np.sum(sonogram[10:,i]) / (f_index.sum()-10)
+        
+        # Same as -log(sumLog / sumSon)
+        entropy[i] = (np.log(sumSon - sumLog) / np.log2(f_index.sum() - 10))-1
+
+        # Amplitude
+        amplitude[i] = np.log(sonogram[10:,i]).sum()
+
+    return t, f[f_index], sonogram, goodness_of_fit, frequency_modulation, amplitude_modulation, spectral_derivative, entropy, amplitude
